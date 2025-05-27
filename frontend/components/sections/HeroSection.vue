bebc6eff4eed17f0dc03cd0ca65dd19ea8806c1c
Mijosed <mijosed@gmail.com>
2025-05-27 02:35:48 +0200

    schedule

--- frontend/components/sections/HeroSection.vue
+++ frontend/components/sections/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="text-center py-24 px-4 bg-white relative overflow-hidden">
-      <img src="/assets/images/hero-bg.jpg" alt="Tandem" class="absolute inset-0 w-full h-full object-cover opacity-10" />
+      <img src="/public/alternance.jpg" alt="Tandem" class="absolute inset-0 w-full h-full object-cover opacity-10" />
       <div class="relative z-10">
         <h1 class="text-4xl md:text-6xl font-bold mb-4" data-aos="fade-down">Trouvez votre alternance facilement</h1>
         <p class="text-lg text-gray-600 mb-6" data-aos="fade-up">Avec Tandem, organisez vos candidatures sans stress.</p>
