c4c01c2fe9af4aaa13199907d1f59e0cc65ae0a0
Mijosed <mijosed@gmail.com>
2025-07-22 12:13:45 +0200

    matomo

--- frontend/app.vue
+++ frontend/app.vue
@@ -4,14 +4,27 @@
   </NuxtLayout>
 </template>
 
-<!-- <style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
-</style> -->
+<script setup>
+useHead({
+  script: [
+    {
+      hid: 'matomo',
+      innerHTML: `
+        var _paq = window._paq = window._paq || [];
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="https://tandemssocial.matomo.cloud/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();`,
+      type: 'text/javascript'
+    }
+  ],
+  __dangerouslyDisableSanitizersByTagID: {
+    matomo: ['innerHTML']
+  }
+})
+</script>
